421b33e5ab6b4a77fa2529d98368a45fde325a1d
Daniel Dantas <164400557+danndnts@users.noreply.github.com>
2024-09-30 15:31:14 -0300

    Add files via upload

---
+++ Questao1/Gerente.java
@@ -0,0 +1,21 @@
+public class Gerente extends Funcionario{
+
+    private String projeto;
+
+    public String getProjeto(){
+        return projeto;
+    }
+
+    public void setProjeto(String projeto){
+        this.projeto = projeto;
+    }
+
+    public void informarProjeto(){
+        System.out.println("Informe o projeto: ");
+        setProjeto(scanner2.nextLine());
+
+    }
+
+
+
+}
